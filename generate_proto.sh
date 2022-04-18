b5ce3ffa97e6dcea5e04627c7577903248d9d604
Anna Nikulásdóttir <anna@grammatek.com>
2022-04-18 21:33:29 +0000

    add clone googleapis to generate

--- generate_proto.sh
+++ generate_proto.sh
@@ -12,14 +12,25 @@ if [ "$?" -ne 0 ]; then
 	pip install grpcio-tools
 fi
 
+# clone the google apis
+GOOGLEAPIS_DIR=googleapis/
+if [ ! -d ${GOOGLEAPIS_DIR} ]; then
+  git clone --depth 1 https://github.com/googleapis/googleapis ${GOOGLEAPIS_DIR}
+fi
+
 mkdir -p generated
 
 # In a project where the protos are going to be used, typical parameters would be:
 # -I./proto/
 # --python_out=src/generated/
 # --grpc_python_out=src/generated/
-# proto/services/preprocessing_service.proto
-# proto/messages/text_message.proto
+# tts-frontend-proto/services/preprocessing_service.proto
+# tts-frontend-proto/messages/preprocessing_message.proto
 
-python3 -m grpc_tools.protoc -I. --python_out=generated/ --grpc_python_out=generated/ services/preprocessing_service.proto
-python3 -m grpc_tools.protoc -I. --python_out=generated/ --grpc_python_out=generated/ messages/text_message.proto
+python3 -m grpc_tools.protoc -I./googleapis -I. --python_out=generated/ \
+        --grpc_python_out=generated/ services/preprocessing_service.proto
+python3 -m grpc_tools.protoc -I./googleapis -I. --python_out=generated/ \
+        --grpc_python_out=generated/ messages/preprocessing_message.proto
+
+#python3 -m grpc_tools.protoc -I. --python_out=generated/ --grpc_python_out=generated/ services/preprocessing_service.proto
+#python3 -m grpc_tools.protoc -I. --python_out=generated/ --grpc_python_out=generated/ messages/preprocessing_message.proto
